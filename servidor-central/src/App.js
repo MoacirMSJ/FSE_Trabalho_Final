81f20b416d423c39fc669cec2f856cdda5320c81
MoacirMSJ <moacirmascarenha86@gmail.com>
2021-05-18 14:08:07 -0300

    Atualização no cardComodo.
    Signed-off-by: MoacirMSJ <moacirmascarenha86@gmail.com>
    Co-authored-by: João Pedro Soares <jpcirqueira81@gmail.com>

--- servidor-central/src/App.js
+++ servidor-central/src/App.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import CardComodo from './components/CardComodo/index.js';
-
+import mqtt from 'mqtt';
 function App() {
+  const [messages, setMessages] = React.useState({});
+  const [comodo, setComodo] = React.useState([]);
+
+
+  const client = mqtt.connect('wss://test.mosquitto.org:8081');
+
+  React.useEffect(() => {
+    
+    client.on('connect', function () { 
+      client.subscribe('fse2020/170080366/dispositivos/#', 
+          function (err) {
+                   if (!err) {
+                      console.log('Conectado')}
+          })   
+    })
+  }, []);
+
+  const trataCadastros =(topic, payload)=>{
+    const m = JSON.parse(payload)
+    if(m.cadastro){
+      setComodo(prev=>[...prev, m.id])
+      console.log(m.id)
+    }
+
+  }
+  
+  client.on('message',trataCadastros);
+
   return (
     <>
-    <h1>teste1</h1>
-    <CardComodo/>
+    <CardComodo comodo="Banheiro" />
+    {/* <p>{comodo.map((message,index) => (
+      <>
+        <CardComodo comodo="Banheiro" />
+      </>  
+     ))}</p> */}
+    
+    
     </>
   );
 }
--- servidor-central/src/components/CardComodo/index.js
+++ servidor-central/src/components/CardComodo/index.js
@@ -1,21 +1,103 @@
 import React from 'react';
-
+import mqtt from 'mqtt';
 import './styles.css'
-function CardComodo() {
+
+const id  = 'f0:8:d1:c5:c6:e4'
+
+function CardComodo(props) {
+  console.log(props)
+
+  const [messages, setMessages] = React.useState({});
+  const [temperatura, setTemperatura] =  React.useState(0);
+  const [umidade, setUmidade] =  React.useState(0);
+  const [estado, setEstado] =  React.useState(0);
+  const [lampada, setLampada] =  React.useState(0);
+  const [alarme, setAlarme] =  React.useState(0);
+
+  const client = mqtt.connect('wss://test.mosquitto.org:8081');
+
+  React.useEffect(() => {
+    
+    client.on('connect', function () { 
+      client.subscribe(`fse2020/170080366/${props.comodo}/#`, 
+          function (err) {
+                   if (!err) {
+                      console.log(`fse2020/170080366/${props.comodo}/#`)
+                      console.log('Conectado')}
+          })   
+    })
+  }, []);
+
+  const gerenciaLampada= ()=>{
+    
+    setLampada(!Boolean(lampada))
+    client.publish(
+      `fse2020/170080366/dispositivos/${id}`,
+      `{ "lampada": ${lampada}}`
+    )
+    console.log(`fse2020/170080366/dispositivos/${id}`)
+  }
+
+  const gerenciaAlarme= ()=>{
+    setAlarme(!Boolean(alarme))
+    client.publish(
+      `fse2020/170080366/dispositivos/${id}`,
+      `{ "alarme": ${alarme}}`
+    )
+    console.log(`fse2020/170080366/dispositivos/${id}`)
+  }
+
+  const gerenciaMensagem =(topic, payload)=>{
+    const m = JSON.parse(payload);
+    if(topic === `fse2020/170080366/${props.comodo}/temperatura` ){
+      if(m.temperatura !== -1){
+        setTemperatura(m.temperatura);
+      }
+      
+      // console("Temp: ",temperatura)
+    }else if(topic === `fse2020/170080366/${props.comodo}/umidade`){
+      if(m.umidade !== -1){
+        setUmidade(m.umidade);  
+      }
+      
+      // console("umidade: ",umidade)
+    }
+    else if(topic === `fse2020/170080366/${props.comodo}/estado`){
+      setEstado(m.estado);
+    }
+  }
+  
+  client.on('message',gerenciaMensagem);
+
   return (
     <div className ='container'>
       <h1 className = "title">
-          comodo: Sala 
+          {props.comodo}
+
        </h1>
 
-      <h2>Temperatura: 30</h2>
-      <h2>umidade: 30%</h2>
+      <h2>Temperatura: {temperatura}°C</h2>
+      <h2>umidade: {umidade}%</h2>
       
       <div className="sub-container">
         <h2>lampada</h2>
+      
+        <input
+            type="checkbox"
+            defaultChecked={lampada}
+  
+            onChange={gerenciaLampada}
+          />
+      
       </div>
       <div className="sub-container">
         <h2>alarme</h2>
+        <input
+            type="checkbox"
+            defaultChecked={alarme}
+
+            onChange={gerenciaAlarme}
+          />
       </div>
     </div>
     
